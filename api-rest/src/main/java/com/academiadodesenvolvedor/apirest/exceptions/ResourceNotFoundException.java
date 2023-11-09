29324f7542d03a66cec302c2e12eb47844b44e40
nildalmo <2nildalmo@gmail.com>
2023-11-09 17:59:51 -0300

    Implementa CRUD para curso

---
+++ api-rest/src/main/java/com/academiadodesenvolvedor/apirest/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.academiadodesenvolvedor.apirest.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
